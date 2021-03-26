5acb9a9b4fd58155bfd3f9b81a878783a609c9da
Cheryl Hoekstra <c_hoekstra@comcast.net>
2021-03-26 11:16:43 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 public static void main(String[] args) {
-System.out.println("Hiya World! Made some changes for ngrok!");
+System.out.println("Hiya World! Made some changes for ngrok2!");
 }
 }
 
